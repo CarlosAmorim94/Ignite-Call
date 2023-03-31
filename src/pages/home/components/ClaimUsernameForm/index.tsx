9d83e9bd31dee0783add111d462dbb87f56a8b6a
carlos <carlosa@xgrow.com>
2023-03-31 10:40:26 -0300

    Feat: Add react-hook-form in home

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,31 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
 import React, { memo } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Form } from "./styles";
 
+const claimUsernameforSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameforSchema>;
+
 export const ClaimUsernameFormComponent = () => {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  const handleClaimUsername = (data: ClaimUsernameFormData) => {
+    console.log(data.username);
+  };
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register("username")}
+      />
       <Button size="sm" type="submit">
         Reservar usuário
         <ArrowRight />
