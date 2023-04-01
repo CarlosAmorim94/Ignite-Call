4429fd6e0384d8251dd0ed14b5507004b2d8604e
carlos <carlosa@xgrow.com>
2023-04-01 16:36:05 -0300

    Feat: Add form verfication with zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,37 +1,57 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { ArrowRight } from "phosphor-react";
-import React, { memo } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Form } from "./styles";
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import React, { memo } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
 
 const claimUsernameforSchema = z.object({
-  username: z.string(),
-});
+  username: z
+    .string()
+    .min(3, { message: 'Mínino de 3 letas.' })
+    .regex(/^([a-z\\\\-]+)$/i, { message: 'Apenas letras e hífens.' })
+    .transform((username) => username.toLowerCase()),
+})
 
-type ClaimUsernameFormData = z.infer<typeof claimUsernameforSchema>;
+type ClaimUsernameFormData = z.infer<typeof claimUsernameforSchema>
 
 export const ClaimUsernameFormComponent = () => {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameforSchema),
+  })
 
   const handleClaimUsername = (data: ClaimUsernameFormData) => {
-    console.log(data.username);
-  };
+    console.log(data.username)
+  }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="seu-usuário"
-        {...register("username")}
-      />
-      <Button size="sm" type="submit">
-        Reservar usuário
-        <ArrowRight />
-      </Button>
-    </Form>
-  );
-};
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar usuário
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
 
-export const ClaimUsernameForm = memo(ClaimUsernameFormComponent);
+export const ClaimUsernameForm = memo(ClaimUsernameFormComponent)
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,9 @@
-import { Heading, Text } from "@ignite-ui/react";
-import { Container, Hero, Preview } from "./styles";
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
 
-import previewImage from "../../assets/app-preview.png";
-import Image from "next/image";
-import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
@@ -26,5 +26,5 @@ export default function Home() {
         />
       </Preview>
     </Container>
-  );
+  )
 }
