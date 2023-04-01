d4b2640f09e2fa7c9ec832af1f91e093e5ed64a4
carlos <carlosa@xgrow.com>
2023-04-01 17:03:29 -0300

    Style: Add style in register page

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,35 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import React from 'react'
+import { Container, Form, Header } from './styles'
+
+export default function registerComponent() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
