9359bcc8800fae2c842802e3cf95285c732e2daf
carlos <carlosa@xgrow.com>
2023-04-05 21:48:29 -0300

    Feat: Add permissions and validate permissions

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,8 +6,24 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar/?error=permissions'
+      }
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
