90b6702cc2a6130e15e9d89b0d67f93e6e263706
carlos <carlosa@xgrow.com>
2023-04-11 15:42:09 -0300

    Feat: Add filterArray in form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,3 +1,5 @@
+import { getWeekDays } from '@/utils/getWeekDays'
+import { zodResolver } from '@hookform/resolvers/zod'
 import {
   Button,
   Checkbox,
@@ -7,6 +9,8 @@ import {
   TextInput,
 } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Container, Header } from '../styles'
 import {
   IntervalBox,
@@ -17,6 +21,41 @@ import {
 } from './styles'
 
 export default function TimeIntervals() {
+  const timeIntervalsFormSchema = z.object({})
+
+  type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enable: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enable: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enable: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enable: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enable: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enable: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enable: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+    resolver: zodResolver(timeIntervalsFormSchema),
+  })
+
+  const weekDays = getWeekDays()
+  const intervals = watch('intervals')
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  const handleSetTimeIntervals = async (data: TimeIntervalsFormData) => {}
+
   return (
     <Container>
       <Header>
@@ -27,18 +66,48 @@ export default function TimeIntervals() {
         </Text>
         <MultiStep size={4} currentStep={3} />
       </Header>
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
-              <TextInput size="sm" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Controller
+                    name={`intervals.${index}.enable`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked)
+                          }}
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+
+                  <Text>{weekDays[field.weekDay]}</Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enable === false}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enable === false}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
         </IntervalContainer>
         <Button type="submit">
           Próximo passo
